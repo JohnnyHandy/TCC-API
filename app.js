edf0bc18831a8085adba33892d3d8f0fe0640ee4
JohnnyHandy <duarterogerpeixoto@gmail.com>
2020-11-26 09:13:03 -0300

    update

--- app.js
+++ app.js
@@ -1,65 +1,41 @@
 var topics = require('./topics')
 var express = require('express')
-var app = express()
-const server = require('http').createServer(app)
 var mqtt = require('mqtt')
-const io =  require('socket.io')(server)
 var path = require('path')
 var cors = require('cors')
+
+var app = express()
+const server = require('http').createServer(app)
+const io =  require('socket.io')(server)
+
+
 const {spawn} = require('child_process');
 
 var client  = mqtt.connect('mqtt://localhost')
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(cors())
 
+const socketEmit = (socket, topic, message) => {
+  return socket.emit('clientSocket', {
+    topic: topic,
+    message: message.toString()
+  })
+}
+const clientPublish = (topic,message) => {
+  return client.publish(topic, message)
+}
 io.sockets.on("connection", (socket) => {
-  const socketEmit = (topic, message) => {
-    return socket.emit('clientSocket', {
-      topic: topic,
-      message: message.toString()
-    })
-  }
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
-  socket.on('closeServer', data => {
-      console.log('Receiving order to close server')
-      process.exit(1)
-  })
-  socket.on('restartServer', data => {
-    console.log('Receiving order to restart server')
-    setTimeout(function () {
-      process.on("exit", function () {
-          require("child_process").spawn(process.argv.shift(), process.argv, {
-              cwd: process.cwd(),
-              detached : true,
-              stdio: "inherit"
-          });
-      });
-      process.exit();
-  }, 5000);
-  })
   socket.on('apiSocket', (response) => {
     const { topic, message } = response
     console.log('topic', topic, 'message', message)
-    client.publish(topic, message)
+    return clientPublish(topic, message)
+    // client.publish(topic, message)
   })
   client.on('message', async function (topic, message) {
-    switch(topic) {
-      case topics.ESP_CONNECTION_SENDSTATUS :
-        return socketEmit(topic, message)
-      case topics.ESP_LED_SENDSTATUS : 
-        return socketEmit(topic, message)
-      case topics.ESP_POT_SENDSTATUS :
-        return socketEmit(topic, message)
-      case topics.ESP_POT_SETCONTROL :
-        return socketEmit(topic, message)
-      case topics.ESP_BUTTON_SENDSTATUS :
-        return socketEmit(topic, message)
-      case topics.ESP_POT_SENDCONTROL :
-        return socketEmit(topic, message)
-      default :
-    }
+    return socketEmit(socket, topic, message)
   })
 });
 
@@ -71,16 +47,33 @@ server.listen(3000, function (){
       }
     })
     client.subscribe(topics.ESP_LED_SENDSTATUS)
-    client.subscribe(topics.ESP_LED_CONTROL)
     client.subscribe(topics.ESP_CONNECTION_SENDSTATUS)
     client.subscribe(topics.ESP_BUTTON_SENDSTATUS)
     client.subscribe(topics.ESP_POT_SETCONTROL)
     client.subscribe(topics.ESP_POT_SENDCONTROL)
     client.subscribe(topics.ESP_POT_SENDSTATUS)
-    client.subscribe(topics.ESP_PWMLED_SENDSTATUS)
   })
   client.on('message', function (topic, message) {
     console.log('Topic: ' + topic + ' Message: ' + message.toString()+'')
   })
    console.log('listening on 3000')
 })
+
+  //Legacy
+  // socket.on('closeServer', data => {
+  //     console.log('Receiving order to close server')
+  //     process.exit(1)
+  // })
+  // socket.on('restartServer', data => {
+  //   console.log('Receiving order to restart server')
+  //   setTimeout(function () {
+  //     process.on("exit", function () {
+  //         require("child_process").spawn(process.argv.shift(), process.argv, {
+  //             cwd: process.cwd(),
+  //             detached : true,
+  //             stdio: "inherit"
+  //         });
+  //     });
+  //     process.exit();
+  // }, 5000);
+  // })
